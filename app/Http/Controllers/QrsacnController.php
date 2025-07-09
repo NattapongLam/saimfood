cf2c7bd34522f6674f2adfb6d5565687c775ed97
saimfood <it.9nat@gmail.com>
2025-07-09 08:18:07 +0700

    ok

--- app/Http/Controllers/QrsacnController.php
+++ app/Http/Controllers/QrsacnController.php
@@ -4,12 +4,23 @@ namespace App\Http\Controllers;
 
 use App\Models\Machine;
 use Illuminate\Http\Request;
+use App\Models\MachineRepairDochd;
+use App\Models\MachinePlaningdocuHd;
+use App\Models\MachineChecksheetDocuHd;
 
 class QrsacnController extends Controller
 {
     public function QrcodeScanMachine($id)
     {
         $mc = Machine::where('machine_code',$id)->first();
-        return view('qr-machine-all',compact('mc'));
+        $checksheet = MachineChecksheetDocuHd::where('machine_code',$id)->where('machine_checksheet_docu_hd_flag',1)->get();
+        $repair = MachineRepairDochd::leftjoin('machine_repair_statuses','machine_repair_dochds.machine_repair_status_id','=','machine_repair_statuses.machine_repair_status_id')
+        ->where('machine_code',$id)->get();
+        $plan = MachinePlaningdocuHd::leftjoin('machine_planingdocu_dts','machine_planingdocu_hds.machine_planingdocu_hd_id','=','machine_planingdocu_dts.machine_planingdocu_hd_id')
+        ->where('machine_planingdocu_hds.machine_planingdocu_hd_flag',true)
+        ->where('machine_planingdocu_dts.machine_planingdocu_dt_flag',true)
+        ->where('machine_code',$id)
+        ->get();
+        return view('qr-machine-all',compact('mc','checksheet','repair','plan'));
     }
 }
--- resources/views/qr-machine-all.blade.php
+++ resources/views/qr-machine-all.blade.php
@@ -22,7 +22,7 @@
         </header>
         <div class="page-content">
            <div class="container-fluid">
-                    <div class="row">
+                    <div class="row p-1">
                         <div class="col-12">
                             <div class="card">
                                 <div class="card-header bg-transparent border-primary">
@@ -63,6 +63,198 @@
                                                 แจ้งซ่อม
                                             </a>
                                         </div>
+                                    </div>
+                                    <br>
+                                    <div class="row">
+                                        <table class="table table-bordered dt-responsive  nowrap w-100 text-center">
+                                            <thead>
+                                                <tr>
+                                                    <th>สถานะ</th>
+                                                    <th>วันที่</th>
+                                                    <th>เลขที่</th>
+                                                    <th>ประเภท</th>
+                                                    <th>ปัญหา</th>
+                                                    <th>ผู้แจ้งซ่อม</th>
+                                                    <th>ผู้ดำเนินการ</th>
+                                                    <th></th>
+                                                    <th>จป.</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                            @foreach ($repair as $item)
+                                                <tr>
+                                                    <td>
+                                                        @if ($item->machine_repair_status_id == 1 || $item->machine_repair_status_id == 9)
+                                                            <span class="badge bg-warning">{{$item->machine_repair_status_name}}</span>                                                           
+                                                        @elseif($item->machine_repair_status_id == 7 || $item->machine_repair_status_id == 8)
+                                                            <span class="badge bg-danger">{{$item->machine_repair_status_name}}</span>   
+                                                        @elseif($item->machine_repair_status_id == 6)
+                                                            <span class="badge bg-success">{{$item->machine_repair_status_name}}</span>  
+                                                        @else
+                                                            <span class="badge bg-secondary">{{$item->machine_repair_status_name}}</span>
+                                                        @endif                                                      
+                                                    </td>
+                                                    <td>{{$item->machine_repair_dochd_date}}</td>
+                                                    <td>{{$item->machine_repair_dochd_docuno}}</td>
+                                                    <td>
+                                                        @if ($item->machine_repair_dochd_type == "ด่วน")
+                                                            <span class="badge badge-soft-danger">งาน :  {{$item->machine_repair_dochd_type}}</span>
+                                                        @else
+                                                            <span class="badge badge-soft-primary">งาน :  {{$item->machine_repair_dochd_type}}</span>
+                                                        @endif
+                                                       
+                                                        @if ($item->repairer_type)
+                                                            @if ($item->repairer_type == "หยุดเครื่อง")
+                                                                <br><span class="badge badge-soft-danger">สถานะ : {{$item->repairer_type}}</span>
+                                                            @else
+                                                                <br><span class="badge badge-soft-primary">สถานะ : {{$item->repairer_type}}</span>
+                                                            @endif
+
+                                                        @endif
+                                                        @if ($item->repairer_problem)
+                                                            @if ($item->repairer_problem == "ควรซื้อใหม่")
+                                                                <br><span class="badge badge-soft-danger">เพิ่มเติม : {{$item->repairer_problem}}</span>
+                                                            @else
+                                                                <br><span class="badge badge-soft-primary">เพิ่มเติม :{{$item->repairer_problem}}</span>
+                                                            @endif
+                                                        
+                                                        @endif
+
+                                                    </td>
+                                                    <td>
+                                                        {{$item->machine_repair_dochd_case}}<br>
+                                                        (ที่ตั้ง : {{$item->machine_repair_dochd_location}})
+                                                    </td>
+                                                    <td>      
+                                                        {{$item->person_at}}                                                
+                                                    </td>
+                                                    <td>  
+                                                        @if ($item->accepting_at)
+                                                            ผู้รับงานซ่อม : {{$item->accepting_at}}  
+                                                        @endif    
+                                                        @if ($item->repairer_at)
+                                                            <br>ผู้ซ่อม : {{$item->repairer_at}}   
+                                                        @endif
+                                                                                                    
+                                                    </td>
+                                                    <td>
+                                                        @if($item->machine_repair_status_id == 6)
+                                                            <a href="{{ route('machine-repair-docus.edit', $item->machine_repair_dochd_id) }}"class="btn btn-primary btn-sm">รายละเอียด</a>
+                                                        @elseif ($item->machine_repair_status_id <> 7 || $item->machine_repair_status_id <> 8)
+                                                            <a href="{{ route('machine-repair-docus.edit', $item->machine_repair_dochd_id) }}"class="btn btn-warning btn-sm"><i class="bx bx-edit-alt"></i> อัพเดท</a>                                
+                                                        @endif                                                      
+                                                    </td>
+                                                    <td>
+                                                        @if($item->safety_at)
+                                                            {{{$item->safety_type}}}<br>
+                                                            ความเห็น : {{$item->safety_note}}
+                                                        @else
+                                                            @if ($item->machine_repair_status_id <> 7 || $item->machine_repair_status_id <> 8)
+                                                            <a href="{{ route('machine-repair-docus.show', $item->machine_repair_dochd_id) }}"class="btn btn-secondary btn-sm">บันทึก</a> 
+                                                            @endif   
+                                                        @endif  
+                                                    </td>
+                                                </tr>
+                                            @endforeach
+                                        </tbody>
+                                        </table>
+                                    </div>
+                                    <br>
+                                    <div class="row">
+                                    <div class="col-xl-12 col-lg-12">
+                                        <div class="card">
+                                            <!-- Nav tabs -->
+                                            <ul class="nav nav-tabs nav-tabs-custom justify-content-center pt-2" role="tablist">
+                                                <li class="nav-item" role="presentation">
+                                                    <a class="nav-link active" data-bs-toggle="tab" href="#all-post" role="tab" aria-selected="true">
+                                                    ตรวจเช็คประจำวัน
+                                                    </a>
+                                                </li>
+                                                <li class="nav-item" role="presentation">
+                                                    <a class="nav-link" data-bs-toggle="tab" href="#archive" role="tab" aria-selected="false" tabindex="-1">
+                                                    PM ประจำปี
+                                                    </a>
+                                                </li>
+                                            </ul>
+
+                                            <!-- Tab panes -->
+                                            <div class="tab-content p-4">
+                                                <div class="tab-pane active show" id="all-post" role="tabpanel">
+                                                    <div class="row">
+                                                        <table class="table table-bordered dt-responsive  nowrap w-100 text-center">
+                                                <thead>
+                                                    <tr>
+                                                        <th>วันที่</th>
+                                                        <th>หมายเหตุ</th>
+                                                        <th>ผู้บันทึก</th>
+                                                        <th>ผู้อนุมัติ</th>
+                                                        <th></th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    @foreach ($checksheet as $item)
+                                                        <tr>
+                                                            <td>{{$item->machine_checksheet_docu_hd_date}}</td>
+                                                            <td>{{$item->machine_checksheet_docu_hd_note}}</td>
+                                                            <td>
+                                                            {{$item->person_at}} 
+                                                            </td>
+                                                            <td>
+                                                                {{$item->approved_at}} 
+                                                            </td>
+                                                            <td>
+                                                                <a href="{{ route('machine-checksheet-docus.edit', $item->machine_checksheet_docu_hd_id) }}"class="btn btn-warning btn-sm"><i class="bx bx-edit-alt"></i> อัพเดท</a>                                                                                                                  
+                                                            </td>
+                                                        </tr>
+                                                    @endforeach
+                                                </tbody>
+                                            </table>
+                                            </div>
+                                            </div>
+
+                                            <div class="tab-pane" id="archive" role="tabpanel">
+                                                <table class="table table-bordered dt-responsive  nowrap w-100 text-center">
+                                                    <thead>
+                                                        <tr>
+                                                            <th>วันที่</th>
+                                                            <th>หมายเหตุ</th>
+                                                            <th>Plan</th>
+                                                            <th>Action</th>
+                                                            <th></th>
+                                                        </tr>
+                                                    </thead>
+                                                     <tbody>
+                                            @foreach ($plan as $item)
+                                                <tr>
+                                                    <td>{{$item->machine_planingdocu_dt_date}}</td>
+                                                    <td>{{$item->machine_planingdocu_dt_note}}</td>
+                                                    <td>
+                                                        @if($item->machine_planingdocu_dt_plan)
+                                                            <input class="form-check-input" type="checkbox" id="formCheckcolor1" checked="">
+                                                        @else
+                                                            <input class="form-check-input" type="checkbox" id="formCheckcolor1">
+                                                        @endif  
+                                                    </td>
+                                                    <td>
+                                                        @if($item->machine_planingdocu_dt_action)
+                                                            <input class="form-check-input" type="checkbox" id="formCheckcolor2" checked="">
+                                                        @else
+                                                            <input class="form-check-input" type="checkbox" id="formCheckcolor2">
+                                                        @endif
+                                                    </td>
+                                                    <td>
+                                                        @if($item->machine_planingdocu_dt_action == 0)
+                                                            <a href="{{ route('machine-planing-docus.edit', $item->machine_planingdocu_dt_id) }}"class="btn btn-warning btn-sm"><i class="bx bx-edit-alt"></i> อัพเดท</a>                                                        
+                                                        @endif                                                        
+                                                    </td>
+                                                </tr>
+                                            @endforeach
+                                        </tbody>
+                                                </table>
+                                            </div>
+                                            </div>
+                                </div>
+                            </div>
                                     </div>                                   
                                 </div>
                             </div>
