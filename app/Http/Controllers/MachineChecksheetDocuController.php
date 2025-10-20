9a909b14b8b52837d920d73b8698b1146fcfdf21
NattapongLam <92558627+NattapongLam@users.noreply.github.com>
2025-10-20 10:41:42 +0700

    ok

--- app/Http/Controllers/MachineChecksheetDocuController.php
+++ app/Http/Controllers/MachineChecksheetDocuController.php
@@ -93,25 +93,22 @@ class MachineChecksheetDocuController extends Controller
 
                 MachineChecksheetDocuDt::insert($data);
             }
-            foreach ($request->emp_day as $key => $value) {
-                $data = [
-                    'machine_checksheet_docu_hd_id' => $hd->machine_checksheet_docu_hd_id,
-                    'created_at' => Carbon::now(),
-                ];
-                for ($i = 1; $i <= 31; $i++) {
-                    $empKey  = 'emp_'  . str_pad($i, 2, '0', STR_PAD_LEFT);
-                    $dateKey = 'date_' . str_pad($i, 2, '0', STR_PAD_LEFT);
-
-                    $empVal = $request->emp_day[$i] ?? null;
-
-                    // เก็บรหัสพนักงาน
-                    $data[$empKey] = $empVal;
-
-                    // ถ้าไม่เป็น null ให้บันทึกวันที่ปัจจุบัน
-                    $data[$dateKey] = $empVal ? now() : null;
-                }
-                MachineChecksheetDocuEmp::create($data);
-            }
+            // foreach ($request->emp_day as $key => $value) {
+            //     $data = [
+            //         'machine_checksheet_docu_hd_id' => $hd->machine_checksheet_docu_hd_id,
+            //         'created_at' => Carbon::now(),
+            //     ];
+            //     for ($i = 1; $i <= 31; $i++) {
+            //         $empKey  = 'emp_'  . str_pad($i, 2, '0', STR_PAD_LEFT);
+            //         $dateKey = 'date_' . str_pad($i, 2, '0', STR_PAD_LEFT);
+            //         $empVal = $request->emp_day[$i] ?? null;
+            //         // เก็บรหัสพนักงาน
+            //         $data[$empKey] = $empVal;
+            //         // ถ้าไม่เป็น null ให้บันทึกวันที่ปัจจุบัน
+            //         $data[$dateKey] = $empVal ? now() : null;
+            //     }
+            //     MachineChecksheetDocuEmp::create($data);
+            // }
             DB::commit();
             return redirect()->route('machine-checksheet-docus.index')->with('success', 'บันทึกข้อมูลเรียบร้อย');
         } catch (\Exception $e) {
@@ -144,19 +141,19 @@ class MachineChecksheetDocuController extends Controller
         $hd = MachineChecksheetDocuHd::leftjoin('machines','machine_checksheet_docu_hds.machine_code','=','machines.machine_code')        
         ->findOrFail($id);
         $emp = DB::table('tg_employee_list')->get();
-        // if ($hd->employees()->count() == 0) {
-        //     try {
-        //         DB::beginTransaction();
-        //         MachineChecksheetDocuEmp::create([
-        //             'machine_checksheet_docu_hd_id' => $hd->machine_checksheet_docu_hd_id,
-        //             'created_at' => Carbon::now(),
-        //         ]);
-        //         DB::commit();
-        //     } catch (\Exception $e) {
-        //         DB::rollback();
-        //         dd($e->getMessage());
-        //     }
-        // }
+        if ($hd->employees()->count() == 0) {
+            try {
+                DB::beginTransaction();
+                MachineChecksheetDocuEmp::create([
+                    'machine_checksheet_docu_hd_id' => $hd->machine_checksheet_docu_hd_id,
+                    'created_at' => Carbon::now(),
+                ]);
+                DB::commit();
+            } catch (\Exception $e) {
+                DB::rollback();
+                dd($e->getMessage());
+            }
+        }
         return view('docu-machine.edit-machinechecksheet-docu',compact('hd','emp'));
     }
 
